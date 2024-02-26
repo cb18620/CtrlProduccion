9a655226f67da4443045914527b4e6b395140ebe
cb18620 <cb18620@gmail.com>
2024-02-26 09:17:29 -0400

    releciones elementos maquinaria
    ok

--- 02_Server/Infraestructura/Identity/obj/Debug/net5.0/Identity.AssemblyInfo.cs
+++ 02_Server/Infraestructura/Identity/obj/Debug/net5.0/Identity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd68147885c40de394aca9067967ab47519a46c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b3f3d18740b58d2bd001c351d5f6e1577c870b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
